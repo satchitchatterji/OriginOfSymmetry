b408d2664b5d1382c7f6ac39dc9e7cf8633f8f69
Satchit Chatterji <satchit.chatterji@gmail.com>
2023-11-12 15:23:03 +0100

    [Untested] Changed to fit code of robot_bodybrain_ea_database, still need to change main

--- examples/robot_bodybrain_ea/individual.py
+++ examples/robot_bodybrain_ea/individual.py
@@ -2,12 +2,16 @@
 
 from dataclasses import dataclass
 
+from base import Base
 from genotype import Genotype
+from revolve2.experimentation.optimization.ea import Individual as GenericIndividual
 
 
 @dataclass
-class Individual:
+class Individual(Base, GenericIndividual[Genotype], population_table="population"):
     """An individual in a population."""
+
+    __tablename__ = "individual"
 
     genotype: Genotype
     fitness: float
