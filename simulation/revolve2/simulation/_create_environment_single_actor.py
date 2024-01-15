2e10bf2386aace5750a40cfd8d7592d3a2156944
Michele Vannucci <michele.vannucci20@gmail.com>
2024-01-15 17:02:22 +0100

    Updated steering algorithm

--- simulation/revolve2/simulation/_create_environment_single_actor.py
+++ simulation/revolve2/simulation/_create_environment_single_actor.py
@@ -1,11 +1,12 @@
 from pyrr import Quaternion, Vector3
 from revolve2.actor_controller import ActorController
 
-#from ._environment_actor_controller import EnvironmentActorController
-#from ._environment_steering_controller import EnvironmentActorController
+# from ._environment_actor_controller import EnvironmentActorController
+# from ._environment_steering_controller import EnvironmentActorController
 from ._terrain import Terrain
 from .actor import Actor
 from .running import Environment, PosedActor
+
 """Contains EnvironmentActorController, an environment controller for an environment with a single actor that uses a provided ActorController."""
 
 from revolve2.actor_controller import ActorController
@@ -16,7 +17,7 @@ import numpy.typing as npt
 from matplotlib import pyplot as plt
 
 
-#from revolve2.simulators.mujoco import LocalRunner
+# from revolve2.simulators.mujoco import LocalRunner
 
 
 class EnvironmentActorController(EnvironmentController):
@@ -30,9 +31,12 @@ class EnvironmentActorController(EnvironmentController):
     x_pos: float
     picture_w: int
 
-    def __init__(self, actor_controller: ActorController,
-                target_points: List[Tuple[float]] = [(0.0,0.0)],
-                steer: bool = False) -> None:
+    def __init__(
+        self,
+        actor_controller: ActorController,
+        target_points: List[Tuple[float]] = [(0.0, 0.0)],
+        steer: bool = False,
+    ) -> None:
         """
         Initialize this object.
 
@@ -44,9 +48,16 @@ class EnvironmentActorController(EnvironmentController):
         self.steer = steer
         if steer:
             self.n = 7
-            
 
-    def control(self, dt: float, actor_control: ActorControl, vision_img: npt.ArrayLike, joint_positions=None, current_pos=None, save_pos=False) -> None:
+    def control(
+        self,
+        dt: float,
+        actor_control: ActorControl,
+        vision_img: npt.ArrayLike,
+        joint_positions=None,
+        current_pos=None,
+        save_pos=False,
+    ) -> None:
         """
         Control the single actor in the environment using an ActorController.
 
@@ -57,19 +68,18 @@ class EnvironmentActorController(EnvironmentController):
         """
 
         # vision
-        #vision_img = np.flip(vision_img) # flip the image because its axis are inverted #we are already doing it before passing the image to the controller
+        # vision_img = np.flip(vision_img) # flip the image because its axis are inverted #we are already doing it before passing the image to the controller
 
         self.picture_w = vision_img.shape[1]
         # / vision
 
-
-        # we use color filters to find the next target point 
-        green_filter = vision_img[:,:,1] < 100
-        red_filter = vision_img[:,:,0] > 100
-        blu_filter = vision_img[:,:,2] < 100
+        # we use color filters to find the next target point
+        green_filter = vision_img[:, :, 1] < 100
+        red_filter = vision_img[:, :, 0] > 100
+        blu_filter = vision_img[:, :, 2] < 100
         coords = np.where(green_filter & red_filter & blu_filter)
 
-        target_in_sight = True # vision
+        target_in_sight = True  # vision
         if coords[1].shape[0] > 0:
             print("sphere in sight")
             self.x_pos = np.mean(coords[1])
@@ -81,44 +91,68 @@ class EnvironmentActorController(EnvironmentController):
 
         assert len(targets) == len(joint_positions)
 
-        #print("Test print")
-        #print("targets: ", targets)
+        # print("Test print")
+        # print("targets: ", targets)
         if self.steer and target_in_sight:
-
             core_position = current_pos[:2]
 
             self.is_left = []
             self.is_right = []
 
-            if save_pos: 
-                for joint_pos in joint_positions[1:]:#TODO why are we skipping the first joint?
-                    self.is_left.append(joint_pos[0] > 0.)
-                    self.is_right.append(joint_pos[0] < 0.)
+            if save_pos:
+                for joint_pos in joint_positions[
+                    1:
+                ]:  # TODO why are we skipping the first joint?
+                    self.is_left.append(joint_pos[0] > 0.0)
+                    self.is_right.append(joint_pos[0] < 0.0)
 
             # check if joints are on the left or right
             joint_positions = [c[:2] for c in joint_positions]
 
-            
-
             # compute steering angle and parameters
-            theta = (self.picture_w - self.x_pos) - (self.picture_w / 2)
+            # theta = (self.picture_w - self.x_pos) - (self.picture_w / 2)
+            theta = self.picture_w / 2 - self.x_pos
             g = (((self.picture_w / 2) - abs(theta)) / (self.picture_w / 2)) ** self.n
 
-            
+            old_steering = False
 
-            # apply steering factor TODO shouldn't we apply the steering factor only to the joints that are not already in the right position?
-            for i, (left, right) in enumerate(zip(self.is_left, self.is_right)):
-                if left:
-                    if theta < 0: # if theta is negative the target is on the right
+            if old_steering:
+                # apply steering factor TODO shouldn't we apply the steering factor only to the joints that are not already in the right position?
+                for i, (left, right) in enumerate(zip(self.is_left, self.is_right)):
+                    if (
+                        left and theta < 0
+                    ):  # if theta is negative the target is on the right
                         print("sphere on the right with theta ", theta)
                         print("multiplying dof by ", g)
                         targets[i] *= g
-                elif right:
-                    if theta >= 0: # if theta is positive the target is on the left
+
+                    elif (
+                        right and theta >= 0
+                    ):  # if theta is positive the target is on the left
                         print("sphere on the left with theta ", theta)
                         print("multiplying dof by ", g)
                         targets[i] *= g
-            
+            else:
+
+                for i, (left, right) in enumerate(zip(self.is_left, self.is_right)):
+                    if theta < 0:  # if theta is negative the target is on the right
+                        print("sphere on the right with theta ", theta)
+
+                        if left:  # left joints are sped up
+                            targets[i] = targets[i] / g
+
+                        if right:  # right joints are slowed down
+                            targets[i] *= g
+
+                    elif theta >= 0:  # if theta is positive the target is on the left
+                        print("sphere on the left with theta ", theta)
+
+                        if left:  # left joints are slowed down
+                            targets[i] *= g
+
+                        if right:  # right joints are sped up
+                            targets[i] = targets[i] / g
+
         actor_control.set_dof_targets(0, targets)
 
     def set_picture_w(self, width: int):
@@ -139,7 +173,7 @@ def create_environment_single_actor(
     """
     bounding_box = actor.calc_aabb()
     # vision
-    #env = Environment(EnvironmentActorController(controller, steer=True, target_points=[(LocalRunner.sphere_pos.x, LocalRunner.sphere_pos.y)]))
+    # env = Environment(EnvironmentActorController(controller, steer=True, target_points=[(LocalRunner.sphere_pos.x, LocalRunner.sphere_pos.y)]))
     env = Environment(EnvironmentActorController(controller, steer=True))
     # / vision
     env.static_geometries.extend(terrain.static_geometry)
