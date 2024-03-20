1cfaffafec7a5a1b1745bec1039613636a2cac31
Satchit Chatterji <satchit.chatterji@gmail.com>
2024-03-20 12:15:57 +0100

    New plots

--- examples/robot_bodybrain_ea/config.py
+++ examples/robot_bodybrain_ea/config.py
@@ -1,6 +1,6 @@
 """Configuration parameters for this example."""
 
-DATABASE_FILE = "./database_sym.sqlite"
+DATABASE_FILE = "./big_exp001.sqlite"
 NUM_REPETITIONS = 1
 NUM_SIMULATORS = 8
 POPULATION_SIZE = 10
