367b8f2be31028f5cb0f1127de5c1d54a58ba199
Michele Vannucci <michele.vannucci20@gmail.com>
2024-04-05 13:39:40 +0200

    [MAC] Changed wandb run name scheme to include target and steer

--- examples/robot_bodybrain_ea/main.py
+++ examples/robot_bodybrain_ea/main.py
@@ -419,9 +419,10 @@ def main(exp_parameters_array: list[ExperimentParameters] , repeated_params = Fa
             best_robots = []
             for rep in range(experiment_parameters.evolution_parameters.num_repetitions):
                 config_dict = vars(experiment_parameters.evolution_parameters)
-                target = str(experiment_parameters.evolution_parameters.target_list)
+                _target = str(experiment_parameters.evolution_parameters.target_list)
+                _steer = str(experiment_parameters.evolution_parameters.steer)
                 config_dict.update({"brain_overall":experiment_parameters.brain_multineat_parameters.OverallMutationRate, "body_overall":experiment_parameters.body_multineat_parameters.OverallMutationRate})                
-                wandb.init(project="final_run, config=config_dict, name=f"{target}_{CUR_TIME}_rep_{rep}")
+                wandb.init(project="final_run, config=config_dict, name=f"{_target}_steer_{_steer}_{CUR_TIME}_rep_{rep}")
                 
                 best_robot = run_experiment(session, rep, steer = experiment_parameters.evolution_parameters.steer, record_settings=exp_rs, experiment_parameters=experiment_parameters)
                 best_robots.append(best_robot)
